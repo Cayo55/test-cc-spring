5abe8d5d0019a3e3c03df948bdfb9156ba15ee42
John <u202117377@upc.edu.pe>
2025-06-14 01:23:24 -0500

    fix: test actions gh.

--- src/test/java/pe/edu/upc/managewise/backend/ManagewiseBackendApplicationTests.java
+++ src/test/java/pe/edu/upc/managewise/backend/ManagewiseBackendApplicationTests.java
@@ -1,19 +1,10 @@
 package pe.edu.upc.managewise.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ManagewiseBackendApplicationTests {
-
-	@BeforeAll
-	static void setupEnvironment() {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		System.setProperty("recaptcha.secret", dotenv.get("RECAPTCHA_SECRET_KEY"));
-		System.setProperty("recaptcha.verify.url", dotenv.get("RECAPTCHA_VERIFY_URL"));
-	}
 
 	@Test
 	void contextLoads() {
